1076d4c8d12ff57de2f0b9f16c62ffcad0fd9f4f
mirajgodha@gmail.com <mirajgodha@gmail.com>
2022-10-15 10:51:01 +0000

    changes for tests

--- test_myfunctions.py
+++ test_myfunctions.py
@@ -8,12 +8,12 @@ columnValue = "VVS2"
 
 # Does the table exist?
 def test_tableExists():
-  assert tableExists(tableName, dbName) is True
+  assert myfunctions.tableExists(tableName, dbName) is True
 
 # Does the column exist?
 def test_columnExists():
-  assert columnExists(tableName, dbName, columnName) is True
+  assert myfunctions.columnExists(tableName, dbName, columnName) is True
 
 # Is there at least one row for the value in the specified column?
 def test_numRowsInColumnForValue():
-  assert numRowsInColumnForValue(tableName, dbName, columnName, columnValue) > 0
+  assert myfunctions.numRowsInColumnForValue(tableName, dbName, columnName, columnValue) > 0
